f14bb907f0a94b2f634eb0e268aade3d2180f30b
Alberto Cortés <alcortes@it.uc3m.es>
2009-07-30 03:04:48 +0200

    Basic Makefile and main.cpp templete

---
+++ src/main.cpp
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+#include "utils.h"
+
+void
+usage(void)
+{
+    fprintf(stderr, "Usage:\n\trotella\n");
+    exit(EXIT_FAILURE);
+}
+
+void
+check_args(int argc, char ** argv)
+{
+    UNUSED(argv);
+
+    if (argc != 1)
+        usage();
+}
+
+int
+main(int argc, char ** argv)
+{
+    check_args(argc, argv);
+    exit(EXIT_FAILURE);
+}
