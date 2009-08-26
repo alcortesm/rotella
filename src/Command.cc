9cd739799886ce00763bd4ecce846d95bca30991
Alberto Cortés <alcortes@it.uc3m.es>
2009-08-26 08:53:09 +0200

    CodingStandard: Rename method and attributes of Command

--- src/Command.cc
+++ src/Command.cc
@@ -1,52 +1,58 @@
 #include "Command.h"
+#include "utils.h"
 
-Command::Command(const std::string & s) {
-    _verb = std::string("Verbo");
-    _nargs = 2;
-    _arg[0] = "Arg1";
-    _arg[1] = "Arg2";
-    _arg[2] = "";
-    UNUSED(s);
+Command::Command(const std::string& rCommandStr)
+{
+   mVerb = std::string("Verbo");
+   mNumArgs = 2;
+   mArg[0] = "Arg1";
+   mArg[1] = "Arg2";
+   mArg[2] = "";
+   UNUSED(rCommandStr);
 }
 
-std::string
-Command::verb() const {
-    return _verb;
+const std::string&
+Command::rVerb() const
+{
+   return mVerb;
 }
 
 unsigned int
-Command::nargs() const {
-    return _nargs;
+Command::NumArgs() const
+{
+   return mNumArgs;
 }
 
-std::string
-Command::arg(int i) const {
-    return _arg[i];
+const std::string&
+Command::rArg(int i) const
+{
+   return mArg[i];
 }
 
 std::ostream &
-operator<<(std::ostream & os, Command c) {
-    os << c.verb();
-    for (unsigned int i=0; i<c.nargs(); i++)
-        os << " " << c.arg(i);
-    return os;
+operator<<(std::ostream & os, Command c)
+{
+   os << c.rVerb();
+   for (unsigned int i=0; i<c.NumArgs(); i++) os << " " << c.rArg(i);
+   return os;
 }
 
-std::istream & operator>>(std::istream & is, Command & c) {
-    UNUSED(c);
-    return is;
-}
-
-bool
-operator==(Command & a, Command & b) {
-    UNUSED(a);
-    UNUSED(b);
-    return true;
+std::istream & operator>>(std::istream & is, Command & c)
+{
+   UNUSED(c);
+   return is;
 }
 
 bool
-operator!=(Command & a, Command & b) {
-    return !(a==b);
+operator==(Command & a, Command & b)
+{
+   UNUSED(a);
+   UNUSED(b);
+   return true;
 }
 
-
+bool
+operator!=(Command & a, Command & b)
+{
+   return !(a==b);
+}
--- src/Command.h
+++ src/Command.h
@@ -5,27 +5,24 @@
 #include <ostream>
 #include <string>
 
-#include "utils.h"
-
-class Command {
+class Command
+{
 public:
-    Command(const std::string & s);
-    class Bad_command {}; // exception class
+   Command(const std::string&);
+   class BadCommand {}; // exception class
 
-    std::string verb() const;
-    unsigned int nargs() const;
-    std::string arg(int) const;
-
-    std::string string_rep() const;
+   const std::string&  rVerb() const;
+   unsigned int        NumArgs() const;
+   const std::string&  rArg(int) const;
 
 private:
-    std::string _verb;
-    unsigned int _nargs;
-    std::string _arg[3];
+    std::string  mVerb;
+    unsigned int mNumArgs;
+    std::string  mArg[3];
 };
 
-std::ostream & operator<<(std::ostream & os, Command c);
-std::istream & operator>>(std::istream & is, Command & c);
+std::ostream& operator<<(std::ostream& os, Command c);
+std::istream& operator>>(std::istream& is, Command& c);
 bool operator==(Command & a, Command & b);
 bool operator!=(Command & a, Command & b);
 
