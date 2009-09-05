4d0c37f9b1a9a3ea920af0146142230164d3bbe8
Alberto Cortés <alcortes@it.uc3m.es>
2009-09-05 12:17:02 +0200

    Add very basic connect and send petition to webcache

--- src/main.cc
+++ src/main.cc
@@ -85,6 +85,14 @@ test_command() {
 }
 
 
+inline void
+add_to_msg(char* const msg, size_t* const poffset, const char* string)
+{
+   size_t sz = strlen(string);
+   memcpy(msg+*poffset, string, sz);
+   *poffset += sz;   
+}
+
 void
 initialize(const Url & wc)
 {
@@ -95,15 +103,15 @@ initialize(const Url & wc)
       int tcp_proto;
       {
          // valgrind says that getprotobyname performs an invalid read:
-         //   http://bugs.debian.org/cgi-bin/bugreport.cgi?bug=543379
+         // http://bugs.debian.org/cgi-bin/bugreport.cgi?bug=543379
          // so until it is fixed I will use "6" as the tcp proto value
-         //            struct protoent * pe_p;
-         //            pe_p = my_getprotobyname("tcp");
-         //            if (!pe_p) {
-         //                perror("getprotobyname");
-         //                exit(EXIT_FAILURE);
-         //            }
-         //            tcp_proto = pe_p->p_proto;
+         //   struct protoent * pe_p;
+         // pe_p = my_getprotobyname("tcp");
+         // if (!pe_p) {
+         //   perror("getprotobyname");
+         //   exit(EXIT_FAILURE);
+         // }
+         // tcp_proto = pe_p->p_proto;
          tcp_proto = 6;
       }
       sock = socket(PF_INET, SOCK_STREAM, tcp_proto);
@@ -114,41 +122,64 @@ initialize(const Url & wc)
    }
 
    // connect to webcache
-   //    {
-   //        struct sockaddr server = {0, {0}};
-   //        socklen_t serverlen = sizeof(server);
-   //        {
-   //            struct sockaddr_in server_in = {0, 0, {0}, {0}};
-   //            server_in.sin_family = AF_INET;
-   //            server_in.sin_port = wc.port_nbo();
-   //            server_in.sin_addr = wc.addr();
-   //            memcpy((void *) &server, (const void *) &server_in, sizeof(server_in));
-   //        }
-   //
-   //        int r;
-   //        r = connect(sock, &server, serverlen);
-   //        if (r == -1) {
-   //            perror("connect");
-   //            exit(EXIT_FAILURE);
-   //        }
-   //    }
+   {
+      struct sockaddr server = {0, {0}};
+      socklen_t serverlen = sizeof(server);
+      {
+         struct sockaddr_in server_in = {0, 0, {0}, {0}};
+         server_in.sin_family = AF_INET;
+         server_in.sin_port = wc.PortNbo();
+         server_in.sin_addr = wc.Addr();
+         memcpy((void *) &server, (const void *) &server_in, sizeof(server_in));
+      }
+
+      int r;
+      r = connect(sock, &server, serverlen);
+      if (r == -1) {
+         perror("connect");
+         exit(EXIT_FAILURE);
+      }
+      debug << "Connected to " << wc.Canonical() << std::endl;
+   }
 
    // send GET to obtain list of rotella nodes
-   //    {
-   //        char * msg[100];
-   //        int offset = 0;
-   //
-   //        memcpy(msg+offset, MSG_GET, strlen(MSG_GET));
-   //        offset += strlen(MSG_GET);
-   //
-   //        memcpy(msg+offset, wc.path().c_str(), strlen(wc.path().c_str()));
-   //        offset += strlen(wc.path().c_str());
-   //
-   //        memcpy(msg+offset, MSG_HTTPV, strlen(MSG_HTTPV));
-   //        offset += strlen(MSG_HTTPV);
-   //
-   //        send(sock, msg, offset, NO_SEND_FLAGS);
-   //    }
+   {
+      char msg[100];
+      size_t offset = 0;
+
+      bzero(msg, 100);
+
+      add_to_msg(msg, &offset, MSG_HTTP_GET.c_str());
+      add_to_msg(msg, &offset, MSG_HTTP_SPACE.c_str());
+      add_to_msg(msg, &offset, wc.Path().c_str());
+      add_to_msg(msg, &offset, MSG_HTTP_SPACE.c_str());
+      add_to_msg(msg, &offset, MSG_HTTP_VERSION.c_str());
+      add_to_msg(msg, &offset, MSG_HTTP_END.c_str());
+
+      ssize_t ns;
+      ns = send(sock, msg, offset, NO_SEND_FLAGS);
+      if (-1 == ns) {
+         perror("send");
+         exit(EXIT_FAILURE);
+      }
+      debug << "Sent \"" << msg << "\"" << std::endl;
+   }
+
+   // try to read response
+   {
+      char * msg[100];
+      bzero(msg, 100);
+
+      ssize_t nr;
+      nr = recv(sock, msg, 100, NO_RECV_FLAGS);
+      if (-1 == nr) {
+         perror("recv");
+         exit(EXIT_FAILURE);
+      }
+
+      printf("%s\n", msg);
+   }
+
    // close the socket
    close(sock);
 }
--- src/rotella.h
+++ src/rotella.h
@@ -1,6 +1,8 @@
 #ifndef ROTELLA_H
 #define ROTELLA_H
 
+#include <string>
+
 // protocol constants
 const int GUID_SZ           = 16;   /* bytes, rotella_protocol.txt:95 */
 const int ASCII_CR          = 13;   /* rotella_protocol.txt:140 */
@@ -13,12 +15,15 @@ const int MAX_TTL           = 15;   /* rotella_protocol.txt:335 */
 const int MAX_QUERY_SZ      = 256;  /* bytes, rotella_protocol.txt:344 */
 
 // constant strings for messages
-#define MSG_GET "GET "
-#define MSG_HTTPV " HTTP/1.1"
+const std::string MSG_HTTP_GET("GET");
+const std::string MSG_HTTP_SPACE(" ");
+const std::string MSG_HTTP_VERSION("HTTP/1.1");
+const std::string MSG_HTTP_END("\r\n");
 #define TCP_PROTO "tcp"
 
 // mix constants
 const bool DEF_DEBUG         = false;
 const int NO_SEND_FLAGS      = 0;
+const int NO_RECV_FLAGS      = 0;
 
 #endif
