15c60668e91233532fe8654aa08af2391060006c
Alberto Cortés <alcortes@it.uc3m.es>
2009-09-10 01:15:36 +0200

    New function to get send a request and read a response from an HTTP server

--- src/main.cc
+++ src/main.cc
@@ -1,8 +1,10 @@
+#include <string>
 #include <iostream>
 #include <cstdlib>
 #include <cstdio>
 #include <ctime>
 #include <cassert>
+#include <stdexcept>
 
 #include <sys/socket.h>
 #include <sys/types.h>
@@ -11,6 +13,7 @@
 #include <netdb.h>
 #include <cstring>
 
+#include "Url.h"
 #include "utils.h"
 #include "rotella.h"
 #include "Command.h"
@@ -93,15 +96,18 @@ add_to_msg(char* const msg, size_t* const poffset, const char* string)
    *poffset += sz;   
 }
 
-//std::string *
-//http_get_response(std::string & request)
-//{
-//}
-
-void
-initialize(const Url & wc)
+// TODO add suport to throw exceptions
+// TODO recv should be OK with fragmentation
+//
+// opens a socket, connect to the http server,
+// sends an HTTP/1.0 request, read the response,
+// wait for the server to close the remote socket,
+// close the local socket and return the response
+// as a string.
+// If there is an error, throws an exception.
+std::string *
+http_get_response(const Url & rServer, const std::string & rQuery) throw (std::runtime_error)
 {
-   UNUSED(wc);
    // open the socket
    int sock;
    {
@@ -128,23 +134,23 @@ initialize(const Url & wc)
 
    // connect to webcache
    {
-      struct sockaddr server = {0, {0}};
-      socklen_t serverlen = sizeof(server);
+      struct sockaddr server_sa = {0, {0}};
+      socklen_t server_sa_len = sizeof(server_sa);
       {
          struct sockaddr_in server_in = {0, 0, {0}, {0}};
          server_in.sin_family = AF_INET;
-         server_in.sin_port = wc.PortNbo();
-         server_in.sin_addr = wc.Addr();
-         memcpy((void *) &server, (const void *) &server_in, sizeof(server_in));
+         server_in.sin_port = rServer.PortNbo();
+         server_in.sin_addr = rServer.Addr();
+         memcpy((void *) &server_sa, (const void *) &server_in, sizeof(server_in));
       }
 
       int r;
-      r = connect(sock, &server, serverlen);
+      r = connect(sock, &server_sa, server_sa_len);
       if (r == -1) {
          perror("connect");
          exit(EXIT_FAILURE);
       }
-      debug << "Connected to " << wc.Canonical() << std::endl;
+      debug << "Connected to " << rServer.Canonical() << std::endl;
    }
 
    // send GET to obtain list of rotella nodes
@@ -162,8 +168,8 @@ initialize(const Url & wc)
       // \r\n
       add_to_msg(msg, &offset, MSG_HTTP_GET.c_str());
       add_to_msg(msg, &offset, MSG_HTTP_SPACE.c_str());
-      add_to_msg(msg, &offset, wc.Path().c_str());
-      add_to_msg(msg, &offset, "?hostfile=1");
+      add_to_msg(msg, &offset, rServer.Path().c_str());
+      add_to_msg(msg, &offset, rQuery.c_str());
       add_to_msg(msg, &offset, MSG_HTTP_SPACE.c_str());
       add_to_msg(msg, &offset, MSG_HTTP_VERSION.c_str());
       add_to_msg(msg, &offset, MSG_HTTP_EOL.c_str());
@@ -175,12 +181,12 @@ initialize(const Url & wc)
          perror("send");
          exit(EXIT_FAILURE);
       }
-      debug << "Sent Hostfile request" << std::endl;
+      debug << "Request sent" << std::endl;
       free(msg);
    }
    // try to read response
+   char * msg;
    {
-      char * msg;
       size_t msgsz = 2048;
       msg = (char *) calloc(msgsz, sizeof(char));
       if (!msg) {
@@ -199,7 +205,7 @@ initialize(const Url & wc)
          exit(EXIT_FAILURE);
       }
 
-      printf("Hostfile Response:\n\"%s\"\n", msg);
+      printf("Response received\n");
 
       // read 0 to detect connection closed by the server
       nr = recv(sock, msg, 2048, NO_RECV_FLAGS);
@@ -216,128 +222,51 @@ initialize(const Url & wc)
       debug << "Connection closed by server" << std::endl;
       close (sock);
       debug << "Connection closed locally" << std::endl;
-      free(msg);
    }
 
-   // open the socket
+   // find the data section of the http response
+   char * data;
    {
-      int tcp_proto;
-      {
-         // valgrind says that getprotobyname performs an invalid read:
-         // http://bugs.debian.org/cgi-bin/bugreport.cgi?bug=543379
-         // so until it is fixed I will use "6" as the tcp proto value
-         //   struct protoent * pe_p;
-         // pe_p = my_getprotobyname("tcp");
-         // if (!pe_p) {
-         //   perror("getprotobyname");
-         //   exit(EXIT_FAILURE);
-         // }
-         // tcp_proto = pe_p->p_proto;
-         tcp_proto = 6;
-      }
-      sock = socket(PF_INET, SOCK_STREAM, tcp_proto);
-      if (sock == -1) {
-         perror("socket");
-         exit(EXIT_FAILURE);
-      }
+      data = strstr(msg, "\r\n\r\n");
+      data += strlen("\r\n\r\n");
    }
 
-   // connect to webcache
-   {
-      struct sockaddr server = {0, {0}};
-      socklen_t serverlen = sizeof(server);
-      {
-         struct sockaddr_in server_in = {0, 0, {0}, {0}};
-         server_in.sin_family = AF_INET;
-         server_in.sin_port = wc.PortNbo();
-         server_in.sin_addr = wc.Addr();
-         memcpy((void *) &server, (const void *) &server_in, sizeof(server_in));
-      }
+   std::string * pResponse = new std::string(data);
+   free(msg);
+   return pResponse;
+}
 
-      int r;
-      r = connect(sock, &server, serverlen);
-      if (r == -1) {
-         perror("connect");
-         exit(EXIT_FAILURE);
-      }
-      debug << "Connected to " << wc.Canonical() << std::endl;
+void
+initialize(const Url & rWebCache)
+{
+   std::string* p_query;
+   std::string* p_response;
+
+   p_query = new string("?hostfile=1");
+   debug << "Sending Hostfile request" << std::endl;
+   try {
+      p_response = http_get_response(rWebCache, *p_query);
+   } catch (std::runtime_error & re) {
+      std::cerr << re.what() << endl;
+      exit(EXIT_FAILURE);      
    }
-   // send GET to update the webcache list with our IP
-   {
-      char * msg;
-      size_t msgsz = 1024;
-      msg = (char *) calloc(msgsz, sizeof(char));
-      if (!msg) {
-         perror("calloc");
-         exit(EXIT_FAILURE);
-      }
-      size_t offset = 0;
+   debug << "Hostfile query response: \"" << *p_response << "\"" << std::endl;
+   delete(p_response);
+   delete(p_query);
 
-      // GET /cgi-bin/gwebcache/gcache.cgi?ip=163.117.15.15:3240 HTTP/1.0\r\n
-      // \r\n
-      add_to_msg(msg, &offset, MSG_HTTP_GET.c_str());
-      add_to_msg(msg, &offset, MSG_HTTP_SPACE.c_str());
-      add_to_msg(msg, &offset, wc.Path().c_str());
-      add_to_msg(msg, &offset, "?ip=");
-      add_to_msg(msg, &offset, "163.117.15.15:3240");
-      add_to_msg(msg, &offset, MSG_HTTP_SPACE.c_str());
-      add_to_msg(msg, &offset, MSG_HTTP_VERSION.c_str());
-      add_to_msg(msg, &offset, MSG_HTTP_EOL.c_str());
-      add_to_msg(msg, &offset, MSG_HTTP_EOL.c_str());
-
-      ssize_t ns;
-      ns = send(sock, msg, offset, NO_SEND_FLAGS);
-      if (-1 == ns) {
-         perror("send");
-         exit(EXIT_FAILURE);
-      }
-      debug << "Sent update request" << std::endl;
-      free(msg);
+   p_query = new string("?ip=101.102.103.104:105");
+   debug << "Sending Update request" << std::endl;
+   try {
+      p_response = http_get_response(rWebCache, *p_query);
+   } catch (std::runtime_error & re) {
+      std::cerr << re.what() << endl;
+      exit(EXIT_FAILURE);      
    }
-   // try to read response to update
-   {
-      char * msg;
-      size_t msgsz = 2048;
-      msg = (char *) calloc(msgsz, sizeof(char));
-      if (!msg) {
-         perror("calloc");
-         exit(EXIT_FAILURE);
-      }
+   debug << "Hostfile query response: \"" << *p_response << "\"" << std::endl;
+   delete(p_response);
+   delete(p_query);
 
-      ssize_t nr;
-      nr = recv(sock, msg, 2048, NO_RECV_FLAGS);
-      if (-1 == nr) {
-         perror("recv");
-         exit(EXIT_FAILURE);
-      }
-      if (0 == nr) {
-         fprintf(stderr, "Conection abnormally closed before reading response\n");
-         exit(EXIT_FAILURE);
-      }
-
-      printf("Update Response:\n\"%s\"\n", msg);
-
-      // read 0 to detect connection closed by the server
-      nr = recv(sock, msg, 2048, NO_RECV_FLAGS);
-      if (-1 == nr) {
-         perror("recv");
-         exit(EXIT_FAILURE);
-      }
-      if (0 != nr) {
-         fprintf(stderr, "Data received instead of conection closed by server\n");
-         exit(EXIT_FAILURE);
-      }
-      // nr == 0; connection closed by server
-
-      debug << "Connection closed by server" << std::endl;
-      close(sock);
-      debug << "Connection closed locally" << std::endl;
-      free(msg);
-   }
-
-
-   // close the socket
-   close(sock);
+   return;
 }
 
 int
