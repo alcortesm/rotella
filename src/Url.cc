61044fa74e1dcc88a1abdad1274e3f5fd84bdafe
Alberto Cortés <alcortes@it.uc3m.es>
2009-10-03 01:57:43 +0200

    Add query and anchor support tu Url

--- src/Url.cc
+++ src/Url.cc
@@ -40,7 +40,7 @@ Url::test(void)
       delete wcp;
    }
    {
-      const char * url = "hTTp://gwc.iblinx.net:13/fc";
+      const char * url = "hTTp://gwc.iblinx.net:13/fc?bla.php#foobar";
       Url * wcp;
       try {
          wcp = new Url(url);
@@ -55,6 +55,8 @@ Url::test(void)
       assert(wcp->Host()  == "gwc.iblinx.net");
       assert(wcp->Port()  == 13);
       assert(wcp->Path()  == "/fc");
+      assert(wcp->Query()  == "bla.php");
+      assert(wcp->Anchor()  == "foobar");
       delete wcp;
    }
    {
@@ -91,6 +93,25 @@ Url::test(void)
       assert(wcp->Host()  == "gwc.iblinx.com");
       assert(wcp->Port()  == 2108);
       assert(wcp->Path()  == "");
+      delete wcp;
+   }
+   {
+      const char * url = "hTTp://gwc.iblInx.coM:2108#bla";
+      Url * wcp;
+      try {
+         wcp = new Url(url);
+      } catch (Url::MalformedUrlException & mue) {
+         std::cerr << mue.what() << endl;
+         goto error;
+      } catch (std::bad_alloc & ba) {
+         std::cerr << ba.what() << ": out of memory" << endl;
+         goto error;
+      }
+      assert(wcp->Proto() == "http");
+      assert(wcp->Host()  == "gwc.iblinx.com");
+      assert(wcp->Port()  == 2108);
+      assert(wcp->Path()  == "");
+      assert(wcp->Anchor() == "bla");
       delete wcp;
    }
    {
@@ -282,6 +303,30 @@ Url::test(void)
       ok9:;
       }
    }
+   {
+      const char * proto = "http";
+      const char * host = "it.uc3m.es";
+      uint16_t port = 8080;
+      const char * path = "/alcortes/index.html";
+      const char * query = "test.php";
+      const char * anchor = "bla";
+
+      try {
+         Url url = Url::FromParts(proto, host, path, port, query, anchor);
+         assert(url.Proto() == proto);
+         assert(url.Host()  == host);
+         assert(url.Port()  == port);
+         assert(url.Path()  == path);
+         assert(url.Query()  == query);
+         assert(url.Anchor()  == anchor);
+      } catch (Url::MalformedUrlException & mue) {
+         std::cerr << mue.what() << endl;
+         goto error;
+      } catch (std::bad_alloc & ba) {
+         std::cerr << ba.what() << ": out of memory" << endl;
+         goto error;
+      }
+   }
    return;
  error:
    exit(EXIT_FAILURE);
@@ -305,7 +350,7 @@ prefix_http_colon_slash_slash(char* * sp) throw (bad_alloc) {
 }
 
 void
-parse_url(const string & url, string & proto, string & host, uint16_t & port, string & path)
+parse_url(const string & url, string & proto, string & host, uint16_t & port, string & path, string & query, string & anchor)
    throw (Url::MalformedUrlException, bad_alloc) {
    char * start;
    const char * end;
@@ -327,44 +372,116 @@ parse_url(const string & url, string & proto, string & host, uint16_t & port, st
    const char * mPathend;
    size_t mPathsz;
 
+   bool has_query;
+   char * mQuerystart;
+   const char * mQueryend;
+   size_t mQuerysz;
+
+   bool has_anchor;
+   char * mAnchorstart;
+   const char * mAnchorend;
+   size_t mAnchorsz;
+
+   // c_str copy to play with
    start = strdup(url.c_str());
    if (!start)
       throw bad_alloc(); 
 
+   // simplify comparisons with to_lower
    to_lower(start);
+
+   // add http:// at the beginning if it's not already there
    if (! has_colon_slash_slash(start))
       prefix_http_colon_slash_slash(&start);
    sz = strlen(start);
    end = start + sz;
    //    debug << "start=" << start << endl;
 
+   // find if there is an anchor strip it
+   // find anchor end
+   mAnchorend = end;
+   // find anchor start
+   mAnchorstart = strstr(start, URL_ANCHOR_SEPARATOR);
+   if (mAnchorstart == end-1) { // empty anchor or last character is '#'
+      free(start);
+      throw Url::MalformedUrlException(url + ": empty anchor");
+   }
+   if (mAnchorstart == NULL) { // no anchor
+      has_anchor = false;
+   } else {
+      has_anchor = true;
+      mAnchorstart++; // advance the URL_ANCHOR_SEPARATOR
+      mAnchorsz = mAnchorend - mAnchorstart;
+      anchor = string(mAnchorstart, mAnchorsz);
+      mAnchorstart--; // go back to URL_ANCHOR_SEPARATOR
+      *mAnchorstart = '\0';
+      sz = strlen(start);
+      end = start + sz;
+   }
+
+   // find if there is a query and strip it
+   // find query end
+   mQueryend = end;
+   // find anchor start
+   mQuerystart = strstr(start, URL_QUERY_SEPARATOR);
+   if (mQuerystart == end-1) { // empty query or last character is '#', strip it
+      free(start);
+      throw Url::MalformedUrlException(url + ": empty query");
+   }
+   if (mQuerystart == NULL) { // no query
+      has_query = false;
+   } else { // there is a query, store it and strip it
+      has_query = true;
+      mQuerystart++; // advance the URL_QUERY_SEPARATOR
+      mQuerysz = mQueryend - mQuerystart;
+      query = string(mQuerystart, mQuerysz);
+      mQuerystart--; // go back to URL_ANCHOR_SEPARATOR
+      *mQuerystart = '\0';
+      sz = strlen(start);
+      end = start + sz;
+   }
+
+   // find proto start
    mProtostart = strstr(start, HTTP_PROTO);
    if (mProtostart != start) {
       free(start);
       throw Url::MalformedUrlException(url + ": don't start with \"http\"");
    }
+   // find proto end
    mProtoend = strstr(mProtostart, COLON_SLASH_SLASH);
    if (mProtoend != mProtostart + strlen(HTTP_PROTO)) {
       free(start);
       throw Url::MalformedUrlException(url + ": \"://\" not found after protocol");
    }
+   // get proto
    mProtosz = mProtoend - mProtostart;
    proto = string(mProtostart, mProtosz);
    //    debug << "proto=" << proto << endl;
 
+
+   // find host start
    mHoststart = mProtoend + strlen(COLON_SLASH_SLASH);
+
+
+
+   // find path end
    mPathend = end;
+   // find path start
    mPathstart = strstr(mHoststart, URL_PATH_SEPARATOR);
    if (!mPathstart)
       mPathstart = mPathend;
+   // get path
    mPathsz = mPathend - mPathstart;
    if (mPathsz == 1)
       mPathsz = 0; // if path is "/" there is no path
    path = string(mPathstart, mPathsz);
    //    debug << "path="  << path  << endl; 
 
+   // find port end
    port_end = mPathstart;
+   // find port start
    port_start = strstr(mHoststart, URL_PORT_SEPARATOR);
+   // get port
    if (!port_start) {
       port_start = port_end;
       port = (uint16_t) HTTP_DEFAULT_PORT;
@@ -382,7 +499,9 @@ parse_url(const string & url, string & proto, string & host, uint16_t & port, st
    }
    //    debug << "port="  << port  << endl;
 
+   // find host end
    mHostend = port_start;
+   // get host
    mHostsz = mHostend - mHoststart;
    if (mHostsz == 0) {
       free(start);
@@ -390,7 +509,7 @@ parse_url(const string & url, string & proto, string & host, uint16_t & port, st
    }
    host = string(mHoststart, mHostsz);
    //    debug << "host="  << host  << endl;
-
+   
    free(start);
    return;
 }
@@ -459,12 +578,12 @@ Url::Addr() const throw (Url::NameResolutionException, Url::NetworkException) {
    return mAddr;
 }
 
-Url::Url(const string & url) throw (Url::MalformedUrlException, bad_alloc)
+Url::Url(const string & rUrl) throw (Url::MalformedUrlException, bad_alloc)
    : mIp("")
 {
    mAddr.s_addr=0;
    try {
-      parse_url(url, mProto, mHost, mPort, mPath);
+      parse_url(rUrl, mProto, mHost, mPort, mPath, mQuery, mAnchor);
    } catch (Url::MalformedUrlException & mue) {
       throw;
    } catch (bad_alloc & ba) {
@@ -477,9 +596,45 @@ Url::Url(const string & url) throw (Url::MalformedUrlException, bad_alloc)
    mCanonical.append(URL_PORT_SEPARATOR);
    mCanonical.append(uint16_to_string(mPort));
    mCanonical.append(mPath);
+   if (HasQuery()) {
+      mCanonical.append(URL_QUERY_SEPARATOR);
+      mCanonical.append(mQuery);
+   }
+   if (HasAnchor()) {
+      mCanonical.append(URL_ANCHOR_SEPARATOR);
+      mCanonical.append(mAnchor);
+   }
 
    mPortNbo = htons(mPort);
    debug << *this << endl;
+}
+
+
+Url
+Url::FromParts(const std::string & rProto,
+       const std::string & rHost,
+       const std::string & rPath,
+       const uint16_t      port,
+       const std::string & rQuery,
+       const std::string & rAnchor) throw (Url::MalformedUrlException, std::bad_alloc)
+{
+   string canonical = std::string("");
+   canonical.append(rProto);
+   canonical.append(COLON_SLASH_SLASH);
+   canonical.append(rHost);
+   canonical.append(URL_PORT_SEPARATOR);
+   canonical.append(uint16_to_string(port));
+   canonical.append(rPath);
+   if (! rQuery.empty()) {
+      canonical.append(URL_QUERY_SEPARATOR);
+      canonical.append(rQuery);
+   }
+   if (! rAnchor.empty()) {
+      canonical.append(URL_ANCHOR_SEPARATOR);
+      canonical.append(rAnchor);
+   }
+   Url url(canonical);
+   return url;
 }
 
 std::ostream &
--- src/Url.h
+++ src/Url.h
@@ -11,6 +11,8 @@
 #define COLON_SLASH_SLASH "://"
 #define URL_PORT_SEPARATOR ":"
 #define URL_PATH_SEPARATOR "/"
+#define URL_QUERY_SEPARATOR "?"
+#define URL_ANCHOR_SEPARATOR "#"
 #define HTTP_DEFAULT_PORT 80
 
 class Url
@@ -34,7 +36,13 @@ public:
       NetworkException(std::string s) : std::runtime_error("Network error: " + s) { }
    };
 
-   Url(const std::string & url) throw (Url::MalformedUrlException, std::bad_alloc);
+   Url(const std::string & rUrl) throw (Url::MalformedUrlException, std::bad_alloc);
+   static Url FromParts(const std::string & rProto,
+       const std::string & rHost,
+       const std::string & rPath,
+       const uint16_t      port = 0,
+       const std::string & rQuery  = "",
+       const std::string & rAnchor = "") throw (Url::MalformedUrlException, std::bad_alloc);
 
    const std::string &    Proto() const { return mProto; };
    uint16_t               Port() const { return mPort; };
@@ -43,7 +51,11 @@ public:
    const struct in_addr & Addr() const throw (Url::NameResolutionException, Url::NetworkException);
    const std::string &    Ip() const throw (Url::NameResolutionException, Url::NetworkException);
    const std::string &    Path() const { return mPath; };
-   const std::string &    Canonical() const {return mCanonical; };
+   const std::string &    Query() const { return mQuery; };
+   const std::string &    Anchor() const { return mAnchor; };
+   const std::string &    Canonical() const { return mCanonical; };
+   bool                   HasQuery() const { return ! mQuery.empty(); };
+   bool                   HasAnchor() const { return ! mAnchor.empty(); };
 
    static void            test(void);
 
@@ -55,6 +67,8 @@ private:
    mutable struct in_addr mAddr;
    mutable std::string    mIp;
    std::string            mPath;
+   std::string            mQuery;
+   std::string            mAnchor;
    std::string            mCanonical;
 
    void get_ip_and_addr() const throw (Url::NameResolutionException, Url::NetworkException);
