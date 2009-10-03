1c0613bddf02db08ece4140a1b5f77c10f9e0ee5
Alberto Cortés <alcortes@it.uc3m.es>
2009-10-03 16:19:08 +0200

    Add new http library and move http_get there

---
+++ src/http.h
@@ -0,0 +1,11 @@
+#include "Url.h"
+
+// TODO recv should be OK with fragmentation
+//
+// opens a socket, connect to the http server,
+// sends an HTTP/1.0 request, read the response,
+// wait for the server to close the remote socket,
+// close the local socket and return the response
+// as a string.
+// If there is an error, throws an exception.
+std::string * http_get(const Url::Url & rUrl) throw (std::runtime_error);
