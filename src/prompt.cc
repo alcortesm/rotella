f6377159baf84bea62f383a37a640a25bdad9e8a
Alberto Cortés <alcortes@it.uc3m.es>
2009-10-16 19:06:49 +0200

    Add error checks for pthread_create and join

--- src/prompt.cc
+++ src/prompt.cc
@@ -1,8 +1,10 @@
 #include <pthread.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <errno.h>
 
 #include "prompt.h"
+#include "utils.h"
 
 void *
 bla(void *)
@@ -11,14 +13,38 @@ bla(void *)
 }
 
 void
-launch_prompt(void)
+launch_prompt(void) throw (std::runtime_error)
 {
    pthread_t prompt_thread;
    int r;
    r = pthread_create(&prompt_thread, NULL, &bla, NULL);
-   if (r != 0) {
-      fprintf(stderr, "error on pthread_create!");
-      exit(EXIT_FAILURE);
+   if (r != 0) { // some pthread_create error
+      if (r == EAGAIN) {
+         throw_fname("pthread_create", "not enough system resources");
+      } else {
+         fprintf(stderr, "pthread_create: unknown error\n");
+         exit(EXIT_FAILURE);
+      }
    }
-   pthread_join(prompt_thread, NULL);
+
+   r = pthread_join(prompt_thread, NULL);
+   if (r != 0) { // some pthread_join error
+      switch (r) {
+      case ESRCH:
+         fprintf(stderr, "pthread_join: thread not found\n");
+         exit(EXIT_FAILURE);
+         break;
+      case EINVAL:
+         fprintf(stderr, "pthread_join: detached thread or already joined by another thread\n");
+         exit(EXIT_FAILURE);
+         break;
+      case EDEADLK:
+         fprintf(stderr, "pthread_join: thread tried to join to itself\n");
+         exit(EXIT_FAILURE);
+         break;
+      default:
+         fprintf(stderr, "pthread_join: unknown error\n");
+         exit(EXIT_FAILURE);
+      }
+   }
 }
--- src/prompt.h
+++ src/prompt.h
@@ -1,1 +1,9 @@
-void launch_prompt(void);
+// -*- Mode: c++ -*-
+#ifndef PROMPT_H
+#define PROMPT_H
+
+#include <stdexcept>
+
+void launch_prompt(void) throw (std::runtime_error);
+
+#endif // PROMPT_H
